1dce754367338be54237a5205f0f36c7d38ef7ac
Ritesh <riteshpeepal@gmail.com>
2026-06-22 00:15:49 +0530

    Add Redux slice and components for todo functionality

--- src/app/store.js
+++ src/app/store.js
@@ -1,1 +1,7 @@
-import {configureStore}
+import {configureStore} from '@reduxjs/toolkit';
+import todoReducer from "../features/todo/todoSlice";
+
+export const store = configureStore({
+    reducer: todoReducer,
+    
+})
