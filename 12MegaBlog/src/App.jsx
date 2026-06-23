8c0d75918d08003e4980b21a5a2a53d867bbcdd3
Ritesh Kumar <riteshpeepal@gmail.com>
2026-06-23 17:54:01 +0530

    feat: integrate Tailwind CSS and set up Redux for authentication
    - Added Tailwind CSS and PostCSS configuration for styling.
    - Implemented Redux for user authentication with login and logout actions.
    - Created Header and Footer components for layout structure.
    - Updated App component to manage user state and render layout.
    - Refactored index.css to use Tailwind's utility classes.

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,14 +1,42 @@
-
+import { useState, useEffect } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from "./appwrite/auth"
+import {login, logout} from './store/authSlice'
+import  Header  from './components/Header/Header.jsx'
+import  Footer  from './components/Footer/Footer'
+import {Outlet} from 'react-router-dom'
 
 function App() {
-  // console.log(process.env.REACT_APP_APPWRITE_URL) 
-  console.log(import.meta.env.VITE_APPWRITE_URL) 
-  return (
-    <>
-      <h1>A blog app with appwrite.</h1>
-    </>
-  )
+
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentuser()
+    .then((userData) => {
+        if(userData){
+          dispatch(login({userData}))
+        } else {
+          dispatch(logout())
+        }
+    })  
+    .finally(() => {
+      return setLoading(false)
+    })
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-slate-800 text-white'>
+      <div className='w-full block '>
+        <Header />
+        <main>
+        TODO:  {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (null) 
 }
 
 export default App
--- 12MegaBlog/src/store/authSlice.js
+++ 12MegaBlog/src/store/authSlice.js
@@ -9,6 +9,19 @@ const authSlice = createSlice({
     name: "auth",
     initialState,
     reducers: {
-        
+        login: (state, action) => {
+            state.status = true,
+            state.userData = action.payload.userData;
+        },
+        logout: (state, action) => {
+            state.status = false;
+            state.userData = null;
+        }
     }
 })
+
+// bro yeh login and logout are the actions... so  authSlice.actions is export....
+
+export const {login, logout} = authSlice.actions;
+
+export default authSlice.reducer;
